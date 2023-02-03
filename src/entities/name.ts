7c5eb827ea393d57ffb14f204201f2fea7764671
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-02-03 19:23:30 -0300

    feat: verify name while creating user

---
+++ src/entities/name.ts
@@ -0,0 +1,29 @@
+import { Either, left, right } from "@/shared";
+import { InvalidEmailError } from "./errors/invalid-email-error";
+
+export class Name {
+  private readonly value: string;
+
+  private constructor(name: string) {
+    this.value = name;
+  }
+
+  static create(name: string): Either<InvalidEmailError, Name> {
+    if (Name.validate(name)) {
+      return right(new Name(name));
+    }
+    return left(new InvalidEmailError());
+  }
+
+  static validate(name: string): boolean {
+    if (!name) {
+      return false;
+    }
+
+    if (name.trim().length < 2 || name.trim().length > 256) {
+      return false;
+    }
+
+    return true;
+  }
+}

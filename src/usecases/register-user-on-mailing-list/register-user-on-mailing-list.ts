669c5e8da0e7f9e9fa7cc8bb6ae20bdea6d549b8
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-02-03 19:54:11 -0300

    feat: register valid user to mailing list

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,26 @@
+import { UserData } from "@/entities/user-data";
+import { UserRepository } from "./ports/user-repository";
+import { Either, left, right } from "@/shared";
+import { User } from "@/entities/user";
+import { InvalidNameError } from "@/entities/errors/invalid-name-error";
+import { InvalidEmailError } from "@/entities/errors/invalid-email-error";
+
+export class RegisterUserOnMailingList {
+  constructor(private readonly userRepo: UserRepository) {}
+
+  async execute(
+    request: UserData
+  ): Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError = User.create(request);
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value);
+    }
+
+    if (!(await this.userRepo.exists(request))) {
+      await this.userRepo.add(request);
+    }
+
+    return right(request);
+  }
+}

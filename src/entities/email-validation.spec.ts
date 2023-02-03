96c97e7af7b68c7c8c87e3cdfb53841f19b9c45a
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-02-03 14:09:29 -0300

    test: validate emails with empty local part

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -16,14 +16,19 @@ describe("Email validation", () => {
     expect(Email.validate(email)).toBeTruthy();
   });
 
+  it("should not accept strings larger than 320 chars", () => {
+    const email =
+      "l".repeat(64) + "@" + "d".repeat(128) + "." + "d".repeat(127);
+    expect(Email.validate(email)).toBeFalsy();
+  });
+
   it("should not accept local part larger than 64 chars", () => {
     const email = "l".repeat(65) + "@mail.com";
     expect(Email.validate(email)).toBeFalsy();
   });
 
-  it("should not accept strings larger than 320 chars", () => {
-    const email =
-      "l".repeat(64) + "@" + "d".repeat(128) + "." + "d".repeat(127);
+  it("should not accept empty local part", () => {
+    const email = "@mail.com";
     expect(Email.validate(email)).toBeFalsy();
   });
 

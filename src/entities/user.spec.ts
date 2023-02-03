7f129f7ce72231fc525c85741f271e7f73bbb2cb
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-02-03 19:23:55 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,6 +1,7 @@
 import { left } from "@/shared";
 import { User } from "./user";
 import { InvalidEmailError } from "./errors/invalid-email-error";
+import { InvalidNameError } from "./errors/invalid-name-error";
 
 describe("User domain entity", () => {
   it("should not create user with invalid e-mail address", () => {
@@ -8,5 +9,19 @@ describe("User domain entity", () => {
 
     const error = User.create({ name: "any_name", email: invalidEmail });
     expect(error).toEqual(left(new InvalidEmailError()));
+  });
+
+  it("should not create user with invalid name (too few characters)", () => {
+    const invalidName = "O    ";
+    const error = User.create({ name: invalidName, email: "any@mail.com" });
+
+    expect(error).toEqual(left(new InvalidNameError()));
+  });
+
+  it("should not create user with invalid name (too many characters)", () => {
+    const invalidName = "O".repeat(257);
+    const error = User.create({ name: invalidName, email: "any@mail.com" });
+
+    expect(error).toEqual(left(new InvalidNameError()));
   });
 });

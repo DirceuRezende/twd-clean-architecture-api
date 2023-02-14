7cbf40e3c3be4931d650d3ac2af665970046a720
Dirceu Rezende <dirceu.srezende@gmail.com>
2023-02-14 09:11:24 -0300

    feat: web controller should return 500 when server raises

--- src/web-controllers/register-user-controller.ts
+++ src/web-controllers/register-user-controller.ts
@@ -1,28 +1,32 @@
 import { UserData } from '@/entities'
-import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { HttpRequest, HttpResponse } from '@/web-controllers/ports'
-import { badRequest, created } from '@/web-controllers/utils'
+import { badRequest, created, serverError } from '@/web-controllers/utils'
 import { MissingParamError } from '@/web-controllers/errors'
+import { UseCase } from '@/usecases/ports'
 
 export class RegisterUserController {
-  constructor (private readonly registerUserOnMailingList: RegisterUserOnMailingList) {}
+  constructor (private readonly registerUserOnMailingList: UseCase) {}
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (!(request.body?.name) || !(request.body?.email)) {
-      let missingParam = !(request.body?.name) ? 'name ' : ''
-      missingParam += !(request.body?.email) ? 'email' : ''
-      return badRequest(new MissingParamError(missingParam.trim()))
-    }
+    try {
+      if (!(request.body?.name) || !(request.body?.email)) {
+        let missingParam = !(request.body?.name) ? 'name ' : ''
+        missingParam += !(request.body?.email) ? 'email' : ''
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
 
-    const userData: UserData = request.body
-    const response = await this.registerUserOnMailingList.execute(userData)
+      const userData: UserData = request.body
+      const response = await this.registerUserOnMailingList.execute(userData)
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
 
-    if (response.isRight()) {
-      return created(response.value)
+      if (response.isRight()) {
+        return created(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
